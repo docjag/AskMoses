895cdeb04f1404dd07e02d7e5d563050f14dbd37
sohel <sohel.digitalhive@gmail.com>
2017-05-29 16:06:22 +0200

    split response has been done. Check duplicate is on progress

--- test_check_duplicate.py
+++ test_check_duplicate.py
@@ -3,20 +3,42 @@
 
 # @Author: Sohel Mahmud
 # @Date: 15.05.17
-# @Description: Read the responnses from the csv file of SHARE-EU and 
+# @Description: Read the responses from the csv file of SHARE-EU and 
 #     			Split multiple answers
 
 
 
 import csv
+from collections import OrderedDict
+
+resp_dict = OrderedDict()
+test_map = OrderedDict()
+
 
 with open('clean_SHARE_sample.csv','rb') as fh:
 	lines = csv.reader(fh)
 	lines.next()
 
 	for line in lines:
+
+		share_id = line[0].strip()
+		item = line[1].strip().split('_')[0] + '_' + 'resp' + '_a'
 		qtext = line[5].strip()
 
 		ans = qtext.split('\n')
+
 		for i in range(len(ans)):
-			print ans[i]
+			ans[i] = ans[i].strip()
+			item_resp =  item + str(i + 1)
+
+			if resp_dict.has_key(item_resp):
+				ref = resp_dict[item_resp]
+				print item_resp, resp_dict[item_resp]
+				print ref
+
+
+        	else:
+        		resp_dict[item_resp] = ans[i]
+        		print item_resp, resp_dict[item_resp]
+
+
--- test_split_resp.py
+++ test_split_resp.py
@@ -3,20 +3,35 @@
 
 # @Author: Sohel Mahmud
 # @Date: 15.05.17
-# @Description: Read the responnses from the csv file of SHARE-EU and 
+# @Description: Read the responses from the csv file of SHARE-EU and 
 #     			Split multiple answers
 
 
 
 import csv
+from collections import OrderedDict
+
+resp_dict = OrderedDict()
 
 with open('clean_SHARE_sample.csv','rb') as fh:
 	lines = csv.reader(fh)
 	lines.next()
 
 	for line in lines:
+		item = line[1].strip().split('_')[0] + '_' + 'resp' + '_a'
 		qtext = line[5].strip()
 
 		ans = qtext.split('\n')
 		for i in range(len(ans)):
-			print ans[i]
+			ans[i] = ans[i].strip()
+			item_resp =  item + str(i + 1)
+			print item_resp, ans[i]
+
+			resp_dict[item_resp] = ans[i]
+
+
+for key, value in resp_dict.items():
+	print key+' : '+ value
+
+
+print resp_dict
