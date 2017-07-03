a54ff9ac321d903efc964b098fe01fbea0760895
docjag <sohel.digitalhive@gmail.com>
2017-07-03 06:43:10 +0200

    latest version of share_master.py

--- share_master.py
+++ share_master.py
@@ -98,9 +98,21 @@ def duplicate_check():
 				print item_resp, ans[i], sanity_check2
 
 
-def check_numbering():
 
+def check_numbering(text):
+	# text = '100. ^MN002_Person[1].Name'
+	# text2 = 'We need $455 for the enrollment'
+	# text3 = '(1900..2017)'
 
+	if re.findall('^[0-9]+\.\s.*',text)
+	print test1
+
+	test2 = re.findall('^\([0-9\.]+\)$',text3)
+	print test2
+
+	text4 = 'a'
+	test3 = re.findall('ab*',text4)
+	print test3
 
 # check the fillers: 
 # The fillers are as follows:
