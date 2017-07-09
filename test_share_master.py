db815249f478b5d8939aee3c824e5e5fc4a94aed
docjag <sohel.digitalhive@gmail.com>
2017-07-09 02:14:34 +0200

    almost final version (duplicate check not done) of test_share_master.py

--- test_share_master.py
+++ test_share_master.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8 -*-
 
 # @Author: Sohel Mahmud
-# @Date: 01.07.17
+# @Date: 08.07.17
 # @Python Version: 2.7.13 (Anaconda)
 # @Description: Read the csv file of SHARE-EU and the create the report
 # The sanity checks are as follows:
-# 					1. Empty cells
-# 					2. Duplicates
-#					3. Numbering
-#					4. Translation
+# 					1. Blank cell check
+# 					2. Duplicate cell check
+#					3. Number format check
+#					4. Fillers check
 
 
-
+import re
 import csv
 from collections import OrderedDict
 
@@ -117,15 +117,18 @@ def check_numbering(text):
 	# text2 = 'We need $455 for the enrollment'
 	# text3 = '(1900..2017)'
 
-	test1 = re.findall('^[0-9]+\.\s.*',text)
-	print test1
+	if text == "":
+		return ""
 
-	test2 = re.findall('^\([0-9\.]+\)$',text)
-	print test2
+	if re.findall('^[0-9]+\.\s.*',text):
+		return ""
 
-	text4 = 'a'
-	test3 = re.findall('ab*',text)
-	print test3
+	elif re.findall('^\([0-9\.]+\)$',text):
+		return ""
+
+	else:
+		return "1"
+
 
 # check the fillers: 
 # The fillers are as follows:
@@ -147,16 +150,19 @@ def check_numbering(text):
 def check_fillers(text):
 
 	# For String having -FL- substring
-	fl_words = re.findall('\S+FL\S+', text)
-	print fl_words
+	if re.findall('\S+FL\S+', text):
+		return "1"
 
 	# for [{String}] format regular expression
-	test1 = re.findall('.*[{[^ ]*}]',text)
-	print test1
+	elif re.findall('.*[{[^ ]*}]',text): 
+		return "1"
 
 	# for [{String}] format regular expression
-	test2 = re.findall('.*[[^ ]*]',text)
-	print test2
+	elif re.findall('.*[[^ ]*]',text):
+		return "1"
+
+	else:
+		return ""
 
 
 # function for writing the header in the Report
@@ -172,10 +178,10 @@ def write_report_header():
 				csv_wr.writerow(col_titles)
 
 
-def report_write(item, lang, text_en, text_fl, check1):
+def report_write(item, lang, text_en, text_fl, check1, check2, check3):
 	with open('Report.csv','ab') as cl_fh:
 			csv_wr = csv.writer(cl_fh, quoting=csv.QUOTE_ALL)
-			csv_wr.writerow([item, lang, text_en, text_fl, check1])
+			csv_wr.writerow([item, lang, text_en, text_fl, check1,check2, check3])
 
 
 def split_resp(item, resp_eng, resp_fl):
@@ -201,51 +207,50 @@ def split_resp(item, resp_eng, resp_fl):
 	return resp_dict
 
 
+#################################################
+############ Main body of the program: ##########
+#################################################
 
 with open('clean_SHARE_sample.csv','rb') as fh:
 	lines = csv.reader(fh)
 	first_line = next(lines)
 
 	for line in lines:
-		if line[0] == 3720:
-			break
 
-	name = line[1].split('_')[0] + '_'
+		name = line[1].split('_')[0] + '_'
 
-	eng = 7
-	start = 11
-	end = len(first_line)
-	
-	for fl in range(start,end,4):
-		country = first_line[fl].split(' ')[2].strip('()')
-		lang_id = lang_dict[country]
-
-		print 'lang: ',lang_id
-
-		# For QTEXT
-		item = name + first_line[fl].split(' ')[0]
-		print 'QTEXT: ',item
-		print line[fl]
-
-		report_write(item, lang_id, line[eng], line[fl], blank_check(line[eng]))
+		eng = 7
+		start = 11
+		end = len(first_line)
 		
-		# for IWER
-		item = name + first_line[fl + 1].split(' ')[0].split('_')[0]
-		print item, line[fl + 1]
-		print line[eng + 1] == ''
-		print blank_check(line[eng + 1])
+		for fl in range(start,end,4):
+			country = first_line[fl].split(' ')[2].strip('()')
+			lang_id = lang_dict[country]
 
-		report_write(item, lang_id, line[eng + 1], line[fl + 1], "1")
+			print 'lang: ',lang_id
 
-		
-		# For Response
-		item = name + first_line[fl + 2].split(' ')[0] + '_' + 'resp' + '_a'
+			# For QTEXT
+			item = name + first_line[fl].split(' ')[0]
+			print 'QTEXT: ',item
+			print line[fl]
 
-		#test_dict = split_resp(item, resp)
-		test_dict = split_resp(item, line[eng + 2], line[fl + 2])
+			report_write(item, lang_id, line[eng], line[fl], blank_check(line[eng]), "", check_fillers(line[eng]))
+			
+			# for IWER
+			item = name + first_line[fl + 1].split(' ')[0].split('_')[0]
+			print item, line[fl + 1]
+			print line[eng + 1] == ''
+			print blank_check(line[eng + 1])
 
-		for key, val in test_dict.items():
-			print key, val[1]
-			report_write(key, lang_id, val[0], val[1], blank_check(val[0]))
+			report_write(item, lang_id, line[eng + 1], line[fl + 1], blank_check(line[eng + 1]), "", check_fillers(line[eng + 1]))
 
-		print '\n'
+			# For Response
+			item = name + first_line[fl + 2].split(' ')[0] + '_' + 'resp' + '_a'
+
+			test_dict = split_resp(item, line[eng + 2], line[fl + 2])
+
+			for key, val in test_dict.items():
+				print key, val[1]
+				report_write(key, lang_id, val[0], val[1], blank_check(val[0]), check_numbering(val[0]), check_fillers(val[0]))
+
+			print '\n'
